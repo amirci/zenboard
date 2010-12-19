6fc3a783f496ec29b91886958062c6e469a9ebfb
Amir Gabriel Barylko <amir@barylko.com>
2010-12-18 22:58:11 -0600

    Added :confirmatable to user to be able to send confirmation emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,18 @@ Zenboard::Application.configure do
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+  	:address	=> "smtp.gmail.com",
+  	:port		=> 587,
+  	:domain		=>"zenboard.com",
+  	:user_name	=>"MonsterCoder",
+  	:password	=>"new123456",
+  	:authentication	=>"plain",
+  	:enable_starttls_auto	=> true
+  }
+  
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
