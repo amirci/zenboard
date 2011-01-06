4882401b95d635423cdc7c43314703e5abdcd15a
Amir Gabriel Barylko <amir@barylko.com>
2011-01-06 04:14:31 -0600

    Fixed failing specs

--- spec/controllers/project_config_controller_spec.rb
+++ spec/controllers/project_config_controller_spec.rb
@@ -6,75 +6,86 @@ describe ProjectConfigController do
   # Setup the user and the associated projects
   before(:each) do
     @user = double("MockUser")
-    @projects = []
-    10.times{ @projects << ProjectConfig.make }
+    
+    @configurations = (1..10).inject([]) do |a, i| 
+      config = double("ProjectConfig_#{i}")
+      config.stub!(:api_key).and_return("aaa")
+      a << config
+    end
 
     @user.stub!(:id).and_return(1)
-    @user.stub!(:configurations).and_return(@projects)
+    @user.stub!(:configurations).and_return(@configurations)
     User.should_receive(:find).with(@user.id).and_return(@user)
+
+    @projects = (1..10).inject([]) { |a, i| a << double("project_#{i}") }
+    Project.stub!(:all).and_return(@projects)
   end
   
   # Index action
   it "should list all the projects config" do
     get :index, :user_id => @user.id
+    
     response.should be_success
-    assigns[:configurations].should == @projects
+    assigns[:configurations].should == @configurations
   end
 
   # Searches for projects to add
   it "Should search for projects matching api" do
-    projects = []
-    10.times { |i| projects << double("project_#{i}") }
-    Project.stub!(:all).and_return(projects)
     Project.should_receive(:api_key=).with("aaa")
+
     get :create, :user_id => @user.id, :api_key => "aaa"
-    assigns("projects").should == projects
+    
+    response.should be_success
+    assigns[:projects].should == @projects
     assigns[:api_key].should == "aaa"
   end
   
   it "Should fail to search if api key is empty" do
     get :create, :user_id => @user.id, :api_key => ""
+    
     flash[:error].should == "Sorry, you need an api-key in order to search for projects"
   end
 
   it "Should fail to search if api key is not present" do
     get :create, :user_id => @user.id
+    
     flash[:error].should == "Sorry, you need an api-key in order to search for projects"
   end
     
   it "Should fail to search if api key is invalid" do
     Project.stub!(:all).and_raise("Invalid api key")
     Project.should_receive(:api_key=).with("aaa")
+    
     get :create, :user_id => @user.id, :api_key => "aaa"
+    
     flash[:error].should == 'Can\'t retrieve project information, make sure the key is valid'
   end
 
   # New action
   it "Should create a new configuration" do
-    project = {"project_id" => '1', "name" => 'Project', "api_key" => 'aaa'}
-    projects = (1..10).inject([]) do |array, i| 
-      mock = double("UserConfiguration_#{i}") 
-      mock.stub!(:api_key).and_return('aaa')
-      array << mock
-    end
-    @user.stub!(:configurations).and_return(projects)
-    ProjectConfig.should_receive(:create!).with(project.merge("user" => @user))
-    get :new, :user_id => @user.id, :project => project
+    config = {"name" => "NewConfig", "project_id" => 1, "api_key" => "aaa"}.merge("user" => @user)
+    Project.should_receive(:api_key=).with("aaa")
+    ProjectConfig.should_receive(:create!).with(config)
+    
+    get :new, :user_id => @user.id, :project => config
+    
     flash[:notice].should == 'The new project configuration has been added'
     assigns[:api_key].should == 'aaa'
-    assigns[:configurations].should == projects
+    assigns[:configurations].should == @configurations
+    assigns[:projects].should == @projects 
   end
         
   # Destroy action
   it "Should remove the configuration" do
-    project = @projects.first
-    project.should_receive(:destroy)
-    ProjectConfig.stub!(:find).with(project.id).and_return(project)    
-    @projects.delete(project)
-    @user.stub!(:configurations).and_return(@projects)
-    get :destroy, :user_id => @user.id, :id => project.id
+    config = @configurations.first
+    config.should_receive(:destroy)
+    ProjectConfig.stub!(:find).with(config.id).and_return(config)    
+    @projects.delete(config)
+    
+    get :destroy, :user_id => @user.id, :id => config.id
+    
     response.should be_success
-    assigns("configurations").should == @projects
+    assigns[:configurations].should == @configurations
   end
   
 end
