94290c2d42da94637b765865d39fad51ce534513
Amir Gabriel Barylko <amir@barylko.com>
2011-01-21 07:13:56 -0600

    Added host configuration for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Zenboard::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
+  config.action_mailer.default_url_options = { :host => 'zenboard.heroku.com' }
   config.action_mailer.delivery_method = :smtp
-  
   config.action_mailer.smtp_settings = {
   	:address	 => "smtp.gmail.com",
   	:port		   => 587,
