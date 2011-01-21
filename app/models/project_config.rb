f38a1d62015e21009abf5b56c756ea1a4e7553ea
Amir Gabriel Barylko <amir@barylko.com>
2011-01-21 09:52:37 -0600

    Fixed uniqueness validation

--- app/models/project_config.rb
+++ app/models/project_config.rb
@@ -1,5 +1,5 @@
 class ProjectConfig < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :api_key, :name, :project_id
-  validates_uniqueness_of :project_id, :scope => :api_key
+  validates_uniqueness_of :project_id, :scope => [:api_key, :user_id]
 end
