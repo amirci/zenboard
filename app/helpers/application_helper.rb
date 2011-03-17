a93b5a340ff97784a8591fa326acd2aa368b1da1
Amir Gabriel Barylko <amir@barylko.com>
2011-03-17 14:44:05 -0500

    Added link to stories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+  
+  def story_link(story)
+    link_to(story.id, "https://agilezen.com/project/#{story.project.id}/story/#{story.id}", :target => "_blank")
+  end
+  
 end
