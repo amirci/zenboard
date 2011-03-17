5c704cee2a2f25b2a9dab3fc4ffc1379a42f6929
Amir Gabriel Barylko <amir@barylko.com>
2011-03-17 11:48:31 -0500

    Fixing heroku issue with bluecloth

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'BlueCloth'
+require 'bluecloth'
 
 # Initialize the rails application
 Zenboard::Application.initialize!
