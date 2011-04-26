0ec3897a418bc6f9ab9c4cae04a19bad375d622a
Amir Gabriel Barylko <amir@barylko.com>
2011-04-26 00:22:30 -0500

    Fixed issue with the burn down chart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -74,65 +74,46 @@ module ApplicationHelper
   def burn_down_graph(stories)
     started = stories.collect { |s| s.started_on }.min
 
-    weeks = Week.since(started)
+    weeks = Week.since(started).reverse
 
-    backlog = {}
-    
-    by_week = weeks.inject({}) do |hash, week|
-      hash[week] = stories.find_all { |s| !s.finished_on.nil? && s.finished_on <= week.finish }
-      backlog[week] = stories.find_all { |s| s.created_on <= week.finish }
-      hash
-    end
-    
-    stories.find_all { |s| s.created_on <= Date.today }.each do |s|
-      puts "**** The story #{s.id} was created on #{s.created_on}"
-    end
-    
-    stories.find_all { |s| s.created_on <= weeks.first.finish }.each do |s|
-      puts "**** The story #{s.id} was created on #{s.created_on} and #{weeks.first.finish}"
-    end
-    
-    
-    by_week = by_week.sort { |a, b| b[0] <=> a[0] }
-    
-    totals = by_week.collect { |week, stories| backlog[week].count - stories.count }
-    
-    labels = by_week.collect { |week, stories| week.start.strftime('%b %d')}
-    
-    backlog = backlog.collect { |w, s| s.count }
-    by_week = by_week.collect { |w, s| s.count }
-    puts "**** The backlog values are : #{backlog.inspect}"
-    puts "**** The by week values are : #{by_week.inspect}"
-    puts "**** The weeks values are : #{labels.inspect}"
-    puts "**** The burn down values are : #{totals.inspect}"
+    backlog = []
 
+    completed = []
+    
+    labels = []
+    
+    burn_down = []
+
+    weeks.each do |week|
+      completed << stories.find_all { |s| !s.finished_on.nil? && s.finished_on <= week.finish }
+      backlog   << stories.find_all { |s| s.created_on <= week.finish }
+      labels    << week.start.strftime('%b %d')
+      burn_down << backlog.last.count - completed.last.count
+    end
+    
     title = "Burndown by week"
     
-    if totals.count > 12 
-      
-      title = "Burndown by 2 weeks"
+    if burn_down.count > 12      
+      title = "Burndown by 2 weeks iterations"
       i = 0
       t2 = []
       l2 = []
-      while i < totals.count - 1  
-        t2 << backlog[i+1] - by_week[i+1]
+      while i < burn_down.count - 1  
+        t2 << burn_down[i+1]
         l2 << labels[i]
         i += 2        
       end
-      t2 << backlog.last - by_week.last unless totals.count % 2 == 0
-      l2 << labels.last unless totals.count % 2 == 0
-      totals = t2
+      t2 << burn_down.last unless burn_down.count % 2 == 0
+      l2 << labels.last unless burn_down.count % 2 == 0
+      burn_down = t2
       labels = l2
-
-      puts "**** The burn down values are : #{totals.inspect}"
-      puts "**** The weeks values are : #{labels.inspect}"
     end
     
 
     lc = GoogleChart::BarChart.new("600x250", title, :vertical, false)
-    lc.data "To Do", totals, '4b7399'
-    lc.width_spacing_options :bar_spacing => 40, :bar_width => 25, :group_spacing => totals.count < 8 ? 40 : 15
-    lc.axis :y, :range => [0, totals.max], :font_size => 10, :alignment => :center
+    lc.data "To Do", burn_down, '4b7399'
+    lc.width_spacing_options :bar_spacing => 40, :bar_width => 25, :group_spacing => burn_down.count < 8 ? 40 : 15
+    lc.axis :y, :range => [0, burn_down.max], :font_size => 10, :alignment => :center
     lc.axis :x, :labels => labels, :font_size => 10, :alignment => :center
     lc.to_url
         
