5b8006322090fd8f5180f6647e349404308313e4
Amir Gabriel Barylko <amir@barylko.com>
2011-05-01 02:30:25 -0500

    Changed the syntax of the helper to generate story tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,16 +31,16 @@ module ApplicationHelper
   # Creates a table for the stories grouped by the key in the hash
   def story_tables(story_hash)
     headers = %w{Id Text Size Start Fin. 1\ Pt. Dur. Block Wait AWork Eff}
-    ths = headers.inject(ActiveSupport::SafeBuffer.new) { |c, header| c.concat content_tag(:th, header) } 
+    ths = headers.collect { |header| content_tag(:th, header) }.reduce(:+)
 
     story_hash.inject(ActiveSupport::SafeBuffer.new) do |c, pair|
       key, stories = pair[0], (pair[1].sort_by(&:finished_on).reverse rescue pair[1])
       c << content_tag(:div, nil, :id => "title") do
          link_to_function("+/-", "Element.toggle('div_#{key}')", :class => "hide_show") + yield(key, stories)
-      end
-      c << content_tag(:table, nil, :id => "div_#{key}") do
-        content_tag(:thead, content_tag(:tr, ths)) \
-        << content_tag(:tbody) do
+      end +
+      content_tag(:table, nil, :id => "div_#{key}") do
+        content_tag(:thead, content_tag(:tr, ths)) +
+        content_tag(:tbody) do
           stories.collect do |s|
             content_tag(:tr, nil, :class => "right #{s.color}") do
               content_tag(:td, link_to_story(s)) +
